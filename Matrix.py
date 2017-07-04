9809edfa0b2c18dcb291be433a1e1b9e041487ea
Hao <linghaoo@gmail.com>
2017-07-04 18:13:48 -0400

    Add eliminate impossible combinations (#1)
    * Remove row/col from near complete when it is complete
    
    * Add eliminateImpossibleCombinations method
    
    * Add eliminateImpossibleCombinations method
    
    * Fix complete vectors and near complete vectors out of sync bug
    
    * Check vector and cross vector for violation
    
    * Can not remove element from list while looping
    
    * Fix check consecutive same numbers with None bug; Fix fill in guess number when generating cross vector
    
    * Add some loggings
    
    * Aim big
    
    * Add drawCell helper function
    
    * Draw cell as we go
    
    * Raise exception and stop when spotting an incorrect row/col
    
    * We have a bug here for 12*12, hard, level 1
    
    * Fix bug for getCombos when theres two zeros/ones

--- Matrix.py
+++ Matrix.py
@@ -28,10 +28,13 @@ class Matrix:
                     1: 0
                 }
             )
-        self.completeRows = []
-        self.completeCols = []
-        self.nearCompleteRows = []
-        self.nearCompleteCols = []
+
+        self.vectorTypes = ('row', 'col')
+        self.completeVectors = []
+        self.nearCompleteVectors = []
+
+        # We can guess up to this many missing cells
+        self.maxComboSize = 4
 
         self.load()
         self.countRowsAndCols()
@@ -46,8 +49,6 @@ class Matrix:
             for j in range(self.size):
                 stringValue = cells[i*self.size+j].text
                 intValue = int(stringValue) if stringValue.strip() else None
-                if intValue is not None:
-                    self.totalCount += 1
                 row.append(intValue)
             self.values.append(row)
 
@@ -59,16 +60,16 @@ class Matrix:
             for j in range(self.size):
                 row.append(self.values[i][j])
             print(row)
-            print('Row total count: ' + str(self.count['row'][i]['total']))
-            print('Row zeros count: ' + str(self.count['row'][i][0]))
-            print('Row ones count: ' + str(self.count['row'][i][1]))
-            print('Col total count: ' + str(self.count['col'][i]['total']))
-            print('Col zeros count: ' + str(self.count['col'][i][0]))
-            print('Col ones count: ' + str(self.count['col'][i][1]))
-        print('Complete rows:')
-        print(self.completeRows)
-        print('Complete cols:')
-        print(self.completeCols)
+        #     print('Row total count: ' + str(self.count['row'][i]['total']))
+        #     print('Row zeros count: ' + str(self.count['row'][i][0]))
+        #     print('Row ones count: ' + str(self.count['row'][i][1]))
+        #     print('Col total count: ' + str(self.count['col'][i]['total']))
+        #     print('Col zeros count: ' + str(self.count['col'][i][0]))
+        #     print('Col ones count: ' + str(self.count['col'][i][1]))
+        # print('Complete vectors:')
+        # print(self.completeVectors)
+        # print('Near complete vectors:')
+        # print(self.nearCompleteVectors)
 
     # Draw the matrix on browser
     def draw(self):
@@ -77,25 +78,56 @@ class Matrix:
                 value = self.values[i][j]
                 cellCssId = '#cel_' + str(i+1) + '_' + str(j+1)
                 cell = self.browser.find_element_by_css_selector(cellCssId)
-                if value == 1:
-                    helpers.setCellToOne(self.browser, cell)
-                elif value == 0:
-                    helpers.setCellToZero(self.browser, cell)
+                helpers.drawCell(self.browser, cell, value)
 
-    # Add one cell to the matrix
-    def addOne(self, i, j, value):
+    # Set cell at given position to given value
+    def addCount(self, i, j, value):
+        # Ignore none value
         if value is None:
             return
+
         self.totalCount += 1
         self.count['row'][i][value] += 1
         self.count['row'][i]['total'] += 1
         self.count['col'][j][value] += 1
         self.count['col'][j]['total'] += 1
-        self.updateCurrentRowAndColCompleteness(i, j)
+        self.updateVectorCompleteness('row', i)
+        self.updateVectorCompleteness('col', j)
+
+    # Set cell value and add count
+    def setCell(self, i, j, value):
+        if value is None:
+            return
+        if self.values[i][j] is not None:
+            return
+
+        print('Setting [' + str(i) + ',' + str(j) + '] from '
+              + str(self.values[i][j]) + ' to ' + str(value))
+        self.values[i][j] = value
+        self.addCount(i, j, value)
+
+        cellCssId = '#cel_' + str(i+1) + '_' + str(j+1)
+        cell = self.browser.find_element_by_css_selector(cellCssId)
+        helpers.drawCell(self.browser, cell, value)
 
     # Check if the matrix is complete
     def isComplete(self):
         return self.totalCount == self.size * self.size
+
+    # Check if the matrix solution is correct
+    def isCorrect(self):
+        if not self.isComplete():
+            return False
+        for i in range(self.size):
+            for v in self.vectorTypes:
+                if (self.count[v][i]['total'] != self.size or
+                        self.count[v][i][0] != self.size/2 or
+                        self.count[v][i][1] != self.size/2):
+                    print(v + str(i)
+                            + ' has ' + str(self.count[v][i][0]) + ' zeros'
+                            + ' and ' + str(self.count[v][i][1]) + ' ones')
+                    return False
+        return True
 
     # Check if index is in range
     def indexIsInRange(self, row, col):
@@ -106,7 +138,10 @@ class Matrix:
         return True
 
     # Set the adjacent cells of neighbours to the other number
-    def setNeighbours(self, neighbours, current):
+    def setNeighbours(self, neighbours, current, log=False):
+        if log:
+            print('Before setting neighbours:', self.values[0])
+            print(neighbours)
         for neighbour in neighbours:
             row = neighbour['row']
             col = neighbour['col']
@@ -119,35 +154,221 @@ class Matrix:
                     if (self.indexIsInRange(adjRow, adjCol) and
                             self.values[adjRow][adjCol] is None):
                         # Set the adjcent to the other number
-                        self.values[adjRow][adjCol] = 1 - current
-                        self.addOne(adjRow, adjCol, 1 - current)
+                        if log:
+                            print('Setting cells at (' + str(adjRow) + ', ' + str(adjCol) + ') to ', str(1-current))
+                        self.setCell(adjRow, adjCol, 1 - current)
+
+        if log:
+            print('After setting neighbours:', self.values[0])
 
     # Count the not none cells in rows/columns
     def countRowsAndCols(self):
         for i in range(self.size):
             for j in range(self.size):
                 if self.values[i][j] is not None:
-                    self.addOne(i, j, self.values[i][j])
+                    self.addCount(i, j, self.values[i][j])
 
     # Check for complete and near complete rows/columns
     def updateCompleteness(self):
         for i in range(self.size):
-            self.updateCurrentRowAndColCompleteness(i, i)
+            self.updateVectorCompleteness('row', i)
+            self.updateVectorCompleteness('col', i)
 
     # Check for complete and near complete for current row/column
-    def updateCurrentRowAndColCompleteness(self, i, j):
-        # We can handle up to this many missing cells
-        threshold = 3
-        # This row is completed
-        if self.count['row'][i]['total'] == self.size:
-            self.completeRows.append(i)
-        # This row is near complete
-        elif self.count['row'][i]['total'] + threshold >= self.size:
-            self.nearCompleteRows.append(i)
+    def updateVectorCompleteness(self, vectorType, i):
+        vectorMissingCells = self.size - self.count[vectorType][i]['total']
+        vector = (vectorType, i, vectorMissingCells)
+        # This vector is complete
+        if vectorMissingCells == 0:
+            if self.checkCorrectness(vectorType, i) is False:
+                print(vector)
+                raise Exception(vectorType + str(i) + ' isnt correct, abort!')
+            print(vectorType + str(i) + ' is complete')
+            # Add to complete vector list if does not exist
+            if vector not in self.completeVectors:
+                print('adding to the complete vectors')
+                self.completeVectors.append(vector)
+            # Remove from near complete vector list if exists
+            self.nearCompleteVectors = list(filter(
+                lambda v: not (v[0] == vectorType and v[1] == i),
+                self.nearCompleteVectors))
+        # This vector is near complete
+        elif vectorMissingCells <= self.maxComboSize:
+            oldVector = None
+            for (vv, ii, mm) in self.nearCompleteVectors:
+                if vv == vectorType and ii == i:
+                    oldVector = (vv, ii, mm)
+            if oldVector is not None:
+                self.nearCompleteVectors.remove(oldVector)
+            self.nearCompleteVectors.append(vector)
+            # Sort by number of missing cells
+            self.nearCompleteVectors.sort(key=lambda row: row[2])
 
-        # This col is completed
-        if self.count['col'][j]['total'] == self.size:
-            self.completeCols.append(j)
-        # This col is near complete
-        elif self.count['col'][j]['total'] + threshold >= self.size:
-            self.nearCompleteCols.append(j)
+    # Check if vector is correct
+    # Aka, having equal number of zeros and ones and no missing element
+    def checkCorrectness(self, vectorType, i):
+        if self.count[vectorType][i]['total'] != self.size:
+            return False
+        if self.count[vectorType][i][0] != self.size/2:
+            return False
+        if self.count[vectorType][i][1] != self.size/2:
+            return False
+
+        return True
+
+    # Get all combination given missing number of zeros and ones
+    def getCombos(self, vectorType, i, missingCount):
+        if missingCount == 2:
+            if self.count[vectorType][i][0] == 1:
+                # One zero and one one, no doubt
+                return [
+                    (0, 1),
+                    (1, 0)
+                ]
+        elif missingCount == 3:
+            if self.count[vectorType][i][0] == self.size/2 - 1:
+                # One zero and two ones
+                return [
+                    (1, 1, 0),
+                    (1, 0, 1),
+                    (0, 1, 1)
+                ]
+            elif self.count[vectorType][i][1] == self.size/2 - 1:
+                # One one and two zeros
+                return [
+                    (0, 0, 1),
+                    (0, 1, 0),
+                    (1, 0, 0)
+                ]
+        elif missingCount == 4:
+            if self.count[vectorType][i][0] == self.size/2 - 1:
+                # One zero and three ones
+                return [
+                    (1, 1, 1, 0),
+                    (1, 1, 0, 1),
+                    (1, 0, 1, 1),
+                    (0, 1, 1, 1)
+                ]
+            elif self.count[vectorType][i][0] == self.size/2 - 2:
+                # Two zeros and two ones
+                return [
+                    (0, 0, 1, 1),
+                    (0, 1, 0, 1),
+                    (0, 1, 1, 0),
+                    (1, 1, 0, 0),
+                    (1, 0, 1, 0),
+                    (1, 0, 0, 1)
+                ]
+            elif self.count[vectorType][i][1] == self.size/2 - 1:
+                # One one and three zeros
+                return [
+                    (0, 0, 0, 1),
+                    (0, 0, 1, 0),
+                    (0, 1, 0, 0),
+                    (1, 0, 0, 0)
+                ]
+
+        # print('Failed to create combos:')
+        # print(str(missingCount))
+        # print(vectorType + str(i))
+        # print('num of zeros: ' + str(self.count[vectorType][i][0]))
+        # print('num of ones: ' + str(self.count[vectorType][i][1]))
+        return []
+
+    # Get row/col candidates
+    def getCandidates(self, vectorType, i, missingCount):
+        candidates = []
+        combos = self.getCombos(vectorType, i, missingCount)
+        for combo in combos:
+            count = 0
+            candidate = []
+            for j in range(self.size):
+                (row, col) = self.getRowAndColIndexes(vectorType, i, j)
+                # Load from combo
+                if self.values[row][col] is None:
+                    val = combo[count]
+                    count += 1
+                    isGuess = True
+                # Load from current cell
+                else:
+                    val = self.values[row][col]
+                    isGuess = False
+
+                candidate.append({
+                    'row': row,
+                    'col': col,
+                    'val': val,
+                    'isGuess': isGuess
+                })
+            candidates.append(candidate)
+
+        return candidates
+
+    # Get row and col indexes given vector type
+    def getRowAndColIndexes(self, vectorType, i, j):
+        if vectorType == 'row':
+            return (i, j)
+        elif vectorType == 'col':
+            return (j, i)
+        else:
+            raise Exception('Unknown vector type!')
+
+    # Check if it violates rules
+    def violatesRules(self, vectorType, candidate, log=False):
+        if log:
+            print(self.values)
+        vector = []
+        for x in range(len(candidate)):
+            vector.append(candidate[x]['val'])
+        if self.hasThreeOrMoreConsecutiveSameNumber(vector):
+            return True
+
+        for j in range(len(candidate)):
+            if candidate[j]['isGuess']:
+                crossVector = []
+                for i in range(self.size):
+                    if i == candidate[j][vectorType]:
+                        crossVector.append(candidate[j]['val'])
+                    else:
+                        (row, col) = self.getRowAndColIndexes(vectorType, i, j)
+                        crossVector.append(self.values[row][col])
+                if self.hasThreeOrMoreConsecutiveSameNumber(crossVector):
+                    return True
+
+        return False
+
+    # Check if it has consecutive three or more zeros/ones
+    def hasThreeOrMoreConsecutiveSameNumber(self, vector):
+        print('Checking violation for ', vector)
+        previous = None
+        streak = 0
+        for x in range(len(vector)):
+            current = vector[x]
+            if current == previous and current is not None:
+                streak += 1
+                if streak >= 2:
+                    print('violated!')
+                    return True
+            else:
+                streak = 0
+            previous = current
+
+        print('nope, no violation')
+        return False
+
+    # Check if matrix has duplicated rows/cols
+    def hasDuplicatedVectors(self, vectorType, target):
+        # Get complete vectors of the same type
+        sources = list(filter(
+            lambda x: x[0] == vectorType, self.completeVectors)
+        )
+        # Nothing to compare against
+        if len(sources) == 0:
+            return False
+        # Check target against every source
+        for (v, i, m) in sources:
+            for j in range(len(target)):
+                (row, col) = self.getRowAndColIndexes(vectorType, i, j)
+                if target[j]['val'] != self.values[row][col]:
+                    return False
+        return True
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from Puzzle import Puzzle
 
 puzzle = Puzzle()
 puzzle.set(
-    Puzzle.DIFFICULTY['medium'],
+    Puzzle.DIFFICULTY['hard'],
     Puzzle.LEVEL[1],
-    Puzzle.SIZE['10*10']
+    Puzzle.SIZE['12*12']
 )
 puzzle.play()
